cf296c55a8d6e0acc1937fb7eb6e6f6d7e5e4330
MASA-JAPAN <masaifactoryline@gmail.com>
2020-04-02 23:21:58 +0900

    implement a login validation

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,19 +1,66 @@
 import React from "react";
+import useFormValidation from "./useFormValidation";
+import validateLogin from "./validateLogin";
+
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  password: ""
+};
 
 function Login(props) {
+  const {
+    handleSubmit,
+    handleBlur,
+    handleChange,
+    values,
+    errors,
+    isSubmitting
+  } = useFormValidation(INITIAL_STATE, validateLogin);
   const [login, setLogin] = React.useState(true);
 
   return (
     <div>
       <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
-      <form className="flex flex-column">
+      <form onSubmit={handleSubmit} className="flex flex-column">
         {!login && (
-          <input type="text" placeholder="Your name" autoComplete="off" />
+          <input
+            onChange={handleChange}
+            value={values.name}
+            name="name"
+            type="text"
+            placeholder="Your name"
+            autoComplete="off"
+          />
         )}
-        <input type="email" placeholder="Your email" autoComplete="off" />
-        <input type="password" placeholder="Choose a secure password" />
+        <input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.email}
+          name="email"
+          type="email"
+          className={errors.email && "error-input"}
+          placeholder="Your email"
+          autoComplete="off"
+        />
+        {errors.email && <p className="error-text">{errors.email}</p>}
+        <input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.password}
+          className={errors.password && "error-input"}
+          name="password"
+          type="password"
+          placeholder="Choose a secure password"
+        />
+        {errors.password && <p className="error-text">{errors.password}</p>}
         <div className="flex mt3">
-          <button type="submit" className="button pointer mr2">
+          <button
+            type="submit"
+            className="button pointer mr2"
+            disabled={isSubmitting}
+            style={{ background: isSubmitting ? "grey" : "orange" }}
+          >
             Submit
           </button>
           <button
